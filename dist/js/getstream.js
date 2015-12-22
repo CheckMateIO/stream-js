b6814ebae2c636012de96b57892fe5ce6c1d5180
Alex Katkova <alex@checkmate.io>
2015-12-22 18:37:35 -0500

    Undo previous commit - update Faye
    turns out it was not the cause of the 403 errors from stream.
    updated version has fixes for some minor annoyances

--- dist/js/getstream.js
+++ dist/js/getstream.js
@@ -1884,7 +1884,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	'use strict';
 
 	var Faye = {
-	  VERSION:          '1.1.1',
+	  VERSION:          '1.1.2',
 
 	  BAYEUX_VERSION:   '1.0',
 	  ID_LENGTH:        160,
@@ -3575,8 +3575,8 @@ return /******/ (function(modules) { // webpackBootstrap
 	    if (!this.batching) return Faye.Promise.fulfilled(this.request([message]));
 
 	    this._outbox.push(message);
-	    this._flushLargeBatch();
 	    this._promise = this._promise || new Faye.Promise();
+	    this._flushLargeBatch();
 
 	    if (message.channel === Faye.Channel.HANDSHAKE) {
 	      this.addTimeout('publish', 0.01, this._flush, this);
@@ -4250,7 +4250,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	    var promise = new Faye.Promise();
 
 	    this.callback(function(socket) {
-	      if (!socket) return;
+	      if (!socket || socket.readyState !== 1) return;
 	      socket.send(Faye.toJSON(messages));
 	      Faye.Promise.fulfill(promise, socket);
 	    }, this);
@@ -4527,6 +4527,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	  request: function(messages) {
 	    var xhrClass = Faye.ENV.XDomainRequest ? XDomainRequest : XMLHttpRequest,
 	        xhr      = new xhrClass(),
+	        id       = ++Faye.Transport.CORS._id,
 	        headers  = this._dispatcher.headers,
 	        self     = this,
 	        key;
@@ -4543,6 +4544,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	    var cleanUp = function() {
 	      if (!xhr) return false;
+	      Faye.Transport.CORS._pending.remove(id);
 	      xhr.onload = xhr.onerror = xhr.ontimeout = xhr.onprogress = null;
 	      xhr = null;
 	    };
@@ -4567,10 +4569,17 @@ return /******/ (function(modules) { // webpackBootstrap
 	    };
 
 	    xhr.onprogress = function() {};
+
+	    if (xhrClass === Faye.ENV.XDomainRequest)
+	      Faye.Transport.CORS._pending.add({id: id, xhr: xhr});
+
 	    xhr.send(this.encode(messages));
 	    return xhr;
 	  }
 	}), {
+	  _id:      0,
+	  _pending: new Faye.Set(),
+
 	  isUsable: function(dispatcher, endpoint, callback, context) {
 	    if (Faye.URI.isSameOrigin(endpoint))
 	      return callback.call(context, false);
